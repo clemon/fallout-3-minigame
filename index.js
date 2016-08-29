66f79abc2f10c5af0a410e14499bdcce20e220b8
Chet Lemon <chetjlemon@gmail.com>
2016-08-28 19:20:06 -0700

    finished. for now

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 let prompt = require("prompt");
+let fs = require("fs");
+
+// config vars
+let clusterSize = 3;
+let answerVisible = false;
+let diffDict = {
+    1: { numLetters: [4,5],
+         numWords: [5,6] },
+    2: { numLetters: [6,7],
+         numWords: [7,8] },
+    3: { numLetters: [8,9,10],
+         numWords: [9,10,11] },
+    4: { numLetters: [11,12,13],
+         numWords: [12,13] },
+    5: { numLetters: [14,15],
+         numWords: [14,15] }
+};
+
+// preprocess data: dictionary where keys are length number
+let lengthDict = {};
+fs.readFileSync("words.txt").toString().split("\r\n").forEach((line) => {
+
+    // dont record words outside of length requirement (4-15 letters)
+    if(line.length < 4 || line.length > 15) { return; }
+
+    if(lengthDict[line.length])
+        lengthDict[line.length].push(line.toLowerCase());
+    else
+        lengthDict[line.length] = [line.toLowerCase()];
+
+});
 
 let diffProps = {
     name: "difficulty",
@@ -7,61 +38,84 @@ let diffProps = {
     required: true
 };
 
-// preprocess data: dictionary where keys are length number
-let lengthDict = {};
-require("fs").readFileSync("words.txt").toString().split("\r\n").forEach((line) => {
-
-    // dont record words outside of length requirement (4-15 letters)
-    if(line.length < 4 || line.length > 15) { return; }
-
-    if(lengthDict[line.length])
-        lengthDict[line.length].push(line);
-    else {
-        lengthDict[line.length] = [line];
-    }
-});
-
-// method returns random int within inclusive range: [min, max]
-let randIntInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min +1)) + min;
-}
-
 // user prompt
 prompt.start();
 prompt.get([diffProps], (err, res) => {
-
-    let diffDict = {
-        1: { numLetters: [4,5],
-             numWords: [5,6] },
-        2: { numLetters: [6,7],
-             numWords: [7,8] },
-        3: { numLetters: [8,9,10],
-             numWords: [9,10,11] },
-        4: { numLetters: [11,12,13],
-             numWords: [12,13] },
-        5: { numLetters: [14,15],
-             numWords: [14,15] }
-    };
 
     // grab potential values from difficulty dictionary
     let numLettersArr = diffDict[res.difficulty]["numLetters"];
     let numWordsArr = diffDict[res.difficulty]["numWords"];
 
     // randomly choose from the potential values
-    let numLetters = numLettersArr[randIntInRange(0, numLettersArr.length-1)];
-    let numWords = numWordsArr[randIntInRange(0, numWordsArr.length-1)];
+    let numLetters = randElem(numLettersArr);
+    let numWords = randElem(numWordsArr);
 
-    // choose words from the length dictionary
     let chosenWords = [];
-    for(let i = 0; i < numWords; i++) {
-        let potentialWords = lengthDict[numLetters]
-        chosenWords.push(potentialWords[randIntInRange(0, potentialWords.length)]);
+    let numClusters = parseInt(numWords/clusterSize);   // number of similar word clusters
+    let remainder = numWords % clusterSize;             // number of extra words to meet length requirement
+    let potentialWords = lengthDict[numLetters];        // all words of the chosen length
+
+    // add clusters to the list
+    for (let i = 0; i < numClusters; i++) {
+
+        // pick random word to start the cluster
+        let clusterStart = randElem(potentialWords);
+        // find all similar words (have enough matching letters)
+        let similarWords = potentialWords.filter((word) => {
+            return numMatches(word, clusterStart) > res.difficulty;
+        });
+        chosenWords.push(clusterStart);             // push the start word
+        for (let j = 0; j < clusterSize; j++) {     // loop clusterSize times
+            // pick random word from the similar list and push
+            let chosenWord = randElem(similarWords);
+            chosenWords.push(chosenWord);
+        }
     }
-    console.log(chosenWords);
-    promptGuess(4, chosenWords, chosenWords[0]);
+    // grab the remainder words randomly
+    for(let i = 0; i < remainder; i++) {
+        chosenWords.push(randElem(potentialWords));
+    }
+
+    let answer = randElem(chosenWords);  // pick the answer randomly
+    if (answerVisible)
+        console.log(`answer: ${answer}`);
+
+    shuffle(chosenWords);
+    console.log("\n");
+    chosenWords.forEach((elm) => { console.log(`  ${elm.toUpperCase()}`); });
+    console.log("\n");
+
+    promptGuess(4, chosenWords, answer);
 });
 
-// Method returns the number of positions matching in the guess and answer
+
+///////////////////// HELPER METHODS /////////////////////
+
+/* randIntInRange(int min, int max)
+ * returns random int within inclusive range: [min, max]
+ */
+let randIntInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+/* randElem(array)
+ * returns a random element within array
+ */
+let randElem = (array) => {
+    return array[randIntInRange(0, array.length-1)];
+}
+
+/* shuffle(array o)
+ * shuffles an array in place
+ */
+let shuffle = (o) => {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+/* numMatches(string guess, string answer)
+ * returns the number of indexes that have matching chars in the guess and answer
+ */
 let numMatches = (guess, answer) => {
     let sum = 0;
     answerArr = answer.split("");
@@ -72,10 +126,12 @@ let numMatches = (guess, answer) => {
     return sum;
 };
 
-
+/* promptGuess(int limit, array words, string answer)
+ * recursively called. Prompts the user for a guess and displays results
+ */
 let promptGuess = (limit, words, answer) => {
 
-    if (limit == 0) {
+    if (limit == 0) {               // if you run out of guesses, the game is over
         console.log("You lose :(");
         return;
     }
@@ -87,7 +143,7 @@ let promptGuess = (limit, words, answer) => {
         required: true
     };
 
-    prompt.get([guessProps], (err, res) => {
+    prompt.get([guessProps], (err, res) => {    // prompt for a guess
 
         // if correct, present win dialogue
         if (res.guess == answer) {
@@ -104,13 +160,3 @@ let promptGuess = (limit, words, answer) => {
         }
     });
 }
-
-
-//TODO: vvv all this shit vvv
-// shuffle the words before displaying
-// pick words less randomly. find words that are more similar to the answer
-    // pick answer randomly, then find words that are similar,
-    // then find words that are not similar. % of words similar depends on diff
-// make sure case is normalized
-// make it look prettier
-// make the code less of a clusterfuck
